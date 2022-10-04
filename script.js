45221140f3eac11b3c6e7393e84adb0fc33ce97a
masugerencias <yannpoirotsanz@gmail.com>
2022-10-04 16:29:47 +0200

    falta bounce

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ 
+
+    setTimeout(function(){
+        $('.bola').css({animation: 'crescendo 2s  both'});
+
+        console.log('time out de un segundo para cuando acabe la primera animacion')
+
+     
+   
+}, 1000);
+
+
+     
+    });
